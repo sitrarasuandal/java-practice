e1959804f58dd427bb684306f781474122a7609d
Akshaya Sitrarasu <sitrarasuandal@gmail.com>
2026-05-03 18:57:09 +0530

    Added Java program to print sum of odd numbers from 1 to 10 using for loop

---
+++ SumOfOddNumbers.java
@@ -0,0 +1,9 @@
+class SumOfOddNumbers{
+    public static void main(String args[]){
+        int sum=0;
+        for(int i=1;i<=10;i+=2){
+            sum=sum+i;
+        }
+        System.out.println("Sum is "+sum);
+    }
+}
