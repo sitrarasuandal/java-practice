ae6fd6614f749b6029099cb54701389c0fe6a66f
Akshaya Sitrarasu <sitrarasuandal@gmail.com>
2026-05-03 18:41:49 +0530

    Added Java program to print odd numbers from 1 to 20 using for loop
    Prints all odd numbers from 1 to 20.

---
+++ OddNumbers1to20.java
@@ -0,0 +1,9 @@
+class OddNumbers1to20{
+    public static void main(String args[]){
+        for(int i=1;i<=20;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
