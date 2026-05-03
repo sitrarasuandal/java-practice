166ad17e62482173f9f0c5a31268cc6baf8cd1b6
Akshaya Sitrarasu <sitrarasuandal@gmail.com>
2026-05-03 18:52:00 +0530

    Added Java program to print sum of even numbers from 1 to 10 using for loop

---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,9 @@
+class SumOfEvenNumbers{
+    public static void main(String args[]){
+        int sum=0;
+        for(int i=2;i<=10;i+=2){
+            sum=sum+i;
+        }
+        System.out.println("Sum is "+sum);
+    }
+}
