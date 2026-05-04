02e86ca1659849ce7a98cee9d395c3c6e4105a0d
Akshaya Sitrarasu <sitrarasuandal@gmail.com>
2026-05-04 09:07:10 +0530

    Are Java program to print multiplication table (2) from 1 to 10 using for loop

---
+++ MultiplicationTable2.java
@@ -0,0 +1,8 @@
+class MultiplicationTable2{
+    public static void main(String args[]){
+        int n=2;
+        for(int i=1;i<=10;i++){
+            System.out.println(n+"x"+i+"="+(n*i));
+        }
+    }
+}
