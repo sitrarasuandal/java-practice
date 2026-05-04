860f8ccc764327049c3ea1c870ef10a2860baf7c
Akshaya Sitrarasu <sitrarasuandal@gmail.com>
2026-05-04 09:33:16 +0530

    Added a java program to count the numbers from 1 to 50

---
+++ CountNumbers1to50.java
@@ -0,0 +1,9 @@
+class CountNumbers1to50{
+    public static void main(String args[]){
+        int count=0;
+        for(int i=1;i<=50;i++){
+            count++;
+        }
+        System.out.println("Count is "+count);
+    }
+}
